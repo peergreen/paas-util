e968675107273a53412af965d5cb60d9c5b0b9cf
Florent Benoit <Florent.Benoit@peergreen.com>
2013-10-25 14:45:36 +0200

    Enable peergreen node in environment template

---
+++ modules/cloud-descriptors/environment-template/node-template/peergreen/src/main/java/org/ow2/jonas/jpaas/util/clouddescriptors/environmenttemplate/nodetemplate/peergreen/PeergreenVersion.java
@@ -0,0 +1,10 @@
+
+package org.ow2.jonas.jpaas.util.clouddescriptors.environmenttemplate.nodetemplate.peergreen;
+
+/**
+ * Peergreen versions
+ * @author Florent Benoit
+ */
+public enum PeergreenVersion {
+    PEERGREEN_1
+}
